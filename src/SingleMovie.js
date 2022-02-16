10b47612bcaf6c1007af7954b18268e30448246b
taranovska <93533926+taranovska@users.noreply.github.com>
2022-02-16 10:56:26 +0200

    add useFetch function

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,29 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
+import useFetch from "./useFetch";
+const url =
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const SingleMovie = () => {
   const { id } = useParams();
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
 
-  const [movie, setMovie] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: "" });
-
-  const fetchMovie = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.Response === "False") {
-      setError({ show: true, msg: data.Error });
-      setIsLoading(false);
-    } else {
-      setMovie(data);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`);
-  }, [id]);
   if (isLoading) {
     return <div className="loading">single movie</div>;
   }
@@ -40,7 +25,7 @@ const SingleMovie = () => {
   const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
   return (
     <section className="single-movie">
-      <img src={poster} alt={title}></img>
+      <img src={poster === "N/A" ? url : poster} alt={title}></img>
       <div className="single-movie-info">
         <h2>{title}</h2>
         <p>{plot}</p>
